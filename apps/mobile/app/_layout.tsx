49f46f834115a3f43cbd099a5c36ecac1c46acea
ElijDavis <elijahadavis@hotmail.com>
2025-10-09 12:59:52 -0400

    Finished tutorial. Added a splash screen, and apparently changed the icon

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,14 +1,14 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <>
-    <StatusBar Bar-style="light-content" />
     <Stack>
       <Stack.Screen name="(tabs)" options={{headerShown: false }} />
       <Stack.Screen name="+not-found" options={{}} />
     </Stack>
+    <StatusBar style="light" />
     </>
     
   );
