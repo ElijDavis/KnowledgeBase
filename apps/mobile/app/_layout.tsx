694f6cae1ef06e0083ddc21bd91709a9d225ca1b
ElijDavis <elijahadavis@hotmail.com>
2025-10-10 18:02:15 -0400

    Expo : Started creating the settings/user bar

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,12 +1,15 @@
-import { Stack } from "expo-router";
+import { Image } from "expo-image";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown: false }} />
-    </Stack>
+      <View className="flex-1 bg-black rounded-full">
+        <View className="bg-[#AF1D1D] float-right rounded-full w-32 h-32">\
+          <Image source={require("../assets/ellipse1.png")} className="w-32 h-32 rounded-full" />
+        </View>
+      </View>
     </SafeAreaProvider>
   );
 }
