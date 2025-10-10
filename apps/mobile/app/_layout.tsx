588bfd1110d34dfed1bcd4006fec0a8ed625df48
ElijDavis <elijahadavis@hotmail.com>
2025-10-10 17:51:28 -0400

    Expo : I deleted everything in the app folder except for the index page and the root layout page

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,16 +1,12 @@
 import { Stack } from "expo-router";
-import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
     <Stack>
-      <Stack.Screen name="(tabs)" options={{headerShown: false }} />
-      <Stack.Screen name="+not-found" options={{}} />
+      <Stack.Screen name="index" options={{headerShown: false }} />
     </Stack>
-    <StatusBar style="light" />
     </SafeAreaProvider>
-    
   );
 }
---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <View className="w-24 h-24 bg-blue-500 rounded-lg shadow-lg" />
+    </View>
+  );
+}
